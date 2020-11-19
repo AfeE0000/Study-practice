1d830d35adfbec7c9ac46803837da7a04d80c6cf
T3Piti <57874182+T3Piti@users.noreply.github.com>
2020-11-19 14:43:01 +0300

    RelayCommand

---
+++ Airport/ViewModels/LoginWindowViewModel.cs
@@ -0,0 +1,36 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+using System.Runtime.CompilerServices;
+using System.ComponentModel;
+
+namespace Airport.ViewModels
+{
+    public class LoginWindowViewModel : INotifyPropertyChanged
+    {
+        //Логин
+        public string Login;
+        private string login 
+        { get
+            { 
+                return Login; 
+            }
+            set 
+            {
+                Login = value;
+                OnPropertyChanged();
+            }
+        }
+
+
+        //Реальизация интерфейся
+        public event PropertyChangedEventHandler PropertyChanged;
+        public void OnPropertyChanged([CallerMemberName] string prop = "")
+        {
+            if (PropertyChanged != null)
+                PropertyChanged(this, new PropertyChangedEventArgs(prop));
+        }
+    }
+}
